a1c517a22ca80e8927a0e77471b32bbcc5f76b88
MickeCardaba <mikael.cardaba@gmail.com>
2024-11-29 13:03:21 +0100

    First commit with Routes started to be created

---
+++ proph/src/Router.tsx
@@ -0,0 +1,21 @@
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      }
+    ],
+    errorElement: <NotFound />,
+  },
+]);
