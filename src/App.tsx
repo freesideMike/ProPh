95da3ff3e8f38d930d04f1abe28f9293f2f510c5
MickeCardaba <mikael.cardaba@gmail.com>
2024-12-05 14:06:17 +0100

    added db with supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import { useState } from "react";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://pzmrtjfxkhgiafjyldjw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6bXJ0amZ4a2hnaWFmanlsZGp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzOTI0MTksImV4cCI6MjA0ODk2ODQxOX0.Y75EwJASRBM8VHxU75gDphS-ewu2-1QCgPLzQ5t24_o"
+);
 
 function App() {
-  const [product, setProduct] = useState([]);
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
+  async function getPhotos() {
+    const { data } = await supabase.from("Photos").select();
+    console.log(data);
+    setPhotos(data);
+  }
 
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <ul>
+        {photos.map((photo) => (<>
+          <li key={photo.id}>{photo.name}</li>
+          <img src={photo.url} alt="" />
+        </>
+        ))}
+      </ul>
+        <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
