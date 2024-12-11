5520cc9ef15cfd5ec3a190580d9835017d601225
MickeCardaba <mikael.cardaba@gmail.com>
2024-12-11 20:00:35 +0100

    center image on OnePhoto

--- src/pages/OnePhoto.tsx
+++ src/pages/OnePhoto.tsx
@@ -3,7 +3,6 @@ import { IPhoto } from "../models/IPhoto";
 import { HeaderSmall } from "../components/HeaderSmall";
 import { Button } from "../components/Button";
 
-
 interface IOnePhotoProps {
   photos: IPhoto[];
 }
@@ -22,37 +21,39 @@ export const OnePhoto: React.FC<IOnePhotoProps> = ({ photos }) => {
     return <div>Photo not found</div>;
   }
 
-   const addToCart = () => {
-     // Logic to add the selected photo to the cart
-     console.log(`Added ${selectedPhoto.title} to cart`);
-     // You can implement your cart logic here
+  const addToCart = () => {
+    // Logic to add the selected photo to the cart
+    console.log(`Added ${selectedPhoto.title} to cart`);
+    // You can implement your cart logic here
   };
-  
+
   return (
     <>
       <HeaderSmall></HeaderSmall>
       <article>
-        <section className="h-8 bg-purple-400">
-          <h3>{selectedPhoto.title}</h3>
-        </section>
-        <section className="w-full aspect-square object-contain align-center justify-center">
+        <section className="flex bg-slate-600  justify-center align-middle mt-8 w-full aspect-square">
           <img
-            className=""
+            className="self-center justify-self-center w-full h-full object-contain"
             key={selectedPhoto.id}
             src={selectedPhoto.url}
             alt={selectedPhoto.title}
           />
         </section>
+        <section className="text-2xl font-mono bg-purple-400">
+          <h3>{selectedPhoto.title}</h3>
+        </section>
         <section className="w-full">
           {selectedPhoto.versions.map((size) => (
-            <ul className="flex flex-row justify-between m-4">
-              <div className="flex flex-row object-left">
-                <li> {size.sizeName} </li>
-                <li> {size.size} </li>
+            <ul className="flex flex-row justify-between m-4 align-bottom">
+              <div className="flex flex-row object-left align-bottom">
+                <li className="text-xl align-bottom ">{size.sizeName}</li>
+                <li className=" align-bottom ms-2 font-semibold text-gray-500 dark:text-gray-400">
+                  {size.size}
+                </li>
               </div>
-              <div className="flex flex-row object-right">
-                <li> {size.price + " kr"} </li>
-                <li>
+              <div className="flex flex-row object-right align-bottom">
+                <li className="align-bottom"> {size.price + " kr"} </li>
+                <li className="align-bottom">
                   <div>
                     <Button click={addToCart} children={"Köp"}></Button>
                   </div>
