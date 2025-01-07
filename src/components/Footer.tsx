8c2f32f878ea0e22afc81478d77df99961277ed1
MickeCardaba <mikael.cardaba@gmail.com>
2025-01-07 10:11:21 +0100

    changed Footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,7 @@ export const Footer = () => {
   return (
     <>
       <footer className="flex flex-row justify-evenly fixed bottom-0 w-full p-3 md:px-16  lg:px-36 xl:px-64 2xl:px-80 md:py-6  lg:py-8 xl:py-12 2xl:py-16 bg-slate-200 border-gray-800 z-10">
-        <p>POWERED</p>
-        <p>by</p>
-        <p>Mikael Cardaba</p>
+        <p>POWERED by Mikael Cardaba</p>
       </footer>
     </>
   );
